70fa8e3854af6e5436b1322dfa0e87d145e8027c
Italo Boss <italo.boss@globant.com>
2021-09-16 16:47:32 -0300

    Add Fish Calculator

---
+++ LocalizationDemo/FishCalc.swift
@@ -0,0 +1,18 @@
+//
+//  FishCalc.swift
+//  LocalizationDemo
+//
+//  Created by Italo Boss Lima Carvalho on 16/09/21.
+//
+
+import Foundation
+
+struct FishCalc {
+
+    let rule = true
+
+    func check(answer: Bool) -> Bool {
+        return answer == rule
+    }
+
+}
--- LocalizationDemo/ViewController.swift
+++ LocalizationDemo/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    @IBOutlet weak var iconLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+
+    @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var denyButton: UIButton!
+
+    // MARK: - Setup
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
     }
 
+    func setupViews() {
+        // Set text for our views
+    }
+
+    // MARK: - Actions
+
+    @IBAction func didTapConfirmButton(_ sender: Any) {
+    }
+
+    @IBAction func didTapDenyButton(_ sender: Any) {
+    }
+
+    private func checkUsersAnswer(isConfirmed: Bool) {
+        let fishCalc = FishCalc()
+        if fishCalc.check(answer: isConfirmed) {
+            // Show message that the user got it right
+        } else {
+            // Show message that the user got it wrong
+        }
+    }
+
+    private func showResult(with message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
-
